bce85eb340720a1492a228a0ee3ee5f61d2d4a53
DanielWeitenauer <daniel@studio-ahoi.de>
2015-09-20 13:12:07 +0200

    Drop adapter

--- config.inc.php
+++ config.inc.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * less.php Plugin 
+ * less.php Plugin
  *
- * @version 1.7.0.4 - 150701
+ * @version 1.7.0.4 - 150724
  * @author Matt Agar
  * @author Martin Jantošovič
  * @author Daniel Weitenauer - studio ahoi
@@ -22,16 +22,36 @@ if (!$REX['SETUP']) {
 	rex_register_extension('SEO42_COMPILE_LESS', 'ahoi_compile_lessdotphp');
 }
 
-function ahoi_compile_lessdotphp($params) 
+function ahoi_compile_lessdotphp($params)
 {
-	require_once (__DIR__.'/lib/lessc.php');
+	require_once (__DIR__.'/lib/Less.php');
 
-	$less = new lessc();
-	$less->setImportDir($params['path']);
-	$less->setFormatter('compressed');
-	$less->setPreserveComments(true);
-	$less->setVariables($params['vars']);
-	$compiledCSS = $less->compile($params['subject']);
+	$compiled_css = '';
 
-	return $compiledCSS;
+	if (!is_array($params['path'])) {
+		$params['path'] = array(
+			 $params['path'] => '',
+		);
+	}
+
+	$options = array(
+		'compress'  => true,
+		'sourceMap' => false,
+		'relativeUrls' => false,
+	);
+
+	try {
+		$parser = new Less_Parser($options);
+		$parser->SetImportDirs($params['path']);
+		$parser->parse($params['subject']);
+		$parser->ModifyVars($params['vars']);
+		$compiled_css = $parser->getCss();
+	} catch(Exception $e) {
+		echo $e->getMessage();
+	}
+
+	// Remove comments
+	$compiled_css = preg_replace('%/\*.*?\*/%s', '', $compiled_css);
+
+	return $compiled_css;
 }
